8a0a06d23137ab6c3eba0bea658985f476844a86
Vinaypotta <vinaypotta@gmail.com>
2025-11-08 22:22:37 +0530

    CppTopics Project

---
+++ CppTopics/CppTopics.h
@@ -0,0 +1,18 @@
+// Documentation for CppTopics.h
+// Project: CPP-Project
+// File: CPP-Project/CppTopics.h
+// Created on: 2024-06-20
+
+// Project Name: CppTopics
+// Type: Learning Resource
+// Description: This file contains various C++ topics and examples for learning purposes.
+// Base class: Base.h
+#pragma once
+#include "Base.h"
+
+class CppTopics : public Base
+{   
+public:
+    CppTopics() = default;
+    ~CppTopics() override = default;
+};
